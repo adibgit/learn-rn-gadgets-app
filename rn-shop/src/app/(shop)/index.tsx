b6d7ec2f5b22b605fc141b1ad0ab104a95875a53
adibsurani <adibsurani@maxis.com.my>
2024-12-25 14:32:33 +0800

    setup cart state, setup zustand library: 1:19:14

---
+++ rn-shop/src/app/(shop)/index.tsx
@@ -0,0 +1,36 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { PRODUCTS } from '../../../assets/products'
+import { ProductListItem } from "../../components/product-list-item"
+import { ListHeader } from '../../components/list-header';
+
+const Home = () => {
+  return (
+    <View>
+      <FlatList 
+        data={PRODUCTS} 
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={styles.flatList}
+      />
+    </View>
+  );
+};
+
+export default Home
+
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: 'space-between',
+  },
+  flatList: {
+    paddingHorizontal: 20,
+    paddingVertical: 5
+  }
+})
